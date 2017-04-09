c5d2a099c22247346317f3e951408db6f394405c
Josh Trygg <jtrygg@gmail.com>
2017-04-09 09:32:12 -0700

    part 1 basic functionality

--- ChatServer.java
+++ ChatServer.java
@@ -4,6 +4,7 @@
 
 import java.net.*;  // socket
 import java.io.*;
+import java.util.*;
 import java.util.Date;
 
 
@@ -11,11 +12,94 @@ public class ChatServer {
 
     public static void main(String args[]) throws java.io.IOException
     {
-        int port = 1300;
-        ServerSocket ss = new ServerSocket(port);
-        Socket connection = ss.accept();
-        System.out.println("I connected!!!!");
-        connection.close();
+    	ArrayList<Connection> clientList = new ArrayList<Connection>();
+    	ArrayList<String> names = new ArrayList<String>();
+    	try
+    	{
+	        int port = 7279;
+	        ServerSocket ss = new ServerSocket(port);
+	        ss.setSoTimeout(500);
 
-    }
+	        while (true)
+	        {
+	        	try
+	        	{
+	        		Socket clientSocket = ss.accept();
+	        		if (clientSocket != null)
+	        		{
+	        			Connection conn = new Connection(clientSocket);
+	        			clientList.add(conn);	        			
+	        			//System.out.println(clientList);
+	        			conn.readMessage();
+	        			System.out.println(conn.getUsername() + " is now connected.");
+	        			
+	        			//conn.writeMessage();
+	        		}
+	        		
+	        	}catch(SocketTimeoutException e){}
+			}
+    	}catch(IOException e) {System.out.println("Listen:" + e.getMessage());}
+	}
+}
+
+
+class Connection 
+{
+	DataInputStream in;
+	DataOutputStream out;
+	Socket clientSocket;
+	String username = "";
+	int reads = 0;
+
+	public Connection (Socket aClientSocket) 
+	{
+		try 
+		{
+			clientSocket = aClientSocket;
+			in = new DataInputStream( clientSocket.getInputStream());
+			out = new DataOutputStream( clientSocket.getOutputStream());
+		}
+		catch(IOException e) {System.out.println("Connection:" + e.getMessage());}
+	}
+
+	public void readMessage()
+	{
+		try
+		{	
+			if (reads == 0)
+			{
+				String data = in.readUTF();
+				this.setUsername(data);
+				this.writeMessage("You are now connected to the ChatServer.");
+				reads++;
+			}
+			else
+			{
+				String data = in.readUTF();	
+				System.out.println("Incoming message..:" + data);
+			}
+			
+		}
+		catch(IOException e) { System.out.println("Connection:" + e.getMessage()); }
+	}
+
+	public void writeMessage(String message)
+	{
+		try 
+		{ 
+			out.writeUTF(message); 
+		}
+		catch(IOException e) { System.out.println("Write Failed."); }
+		
+	}
+
+	public void setUsername(String name)
+	{
+		this.username = name;
+	}
+	public String getUsername()
+	{
+		return this.username;
+	}
+
 }
